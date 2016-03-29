6e72561b9205e50945c1ce7cf84022765382c0b6
Ben Clarke <ben@upliftingmedia.co.uk>
2016-03-29 16:22:13 +0100

    changed static_pages from scss to sass also did some styling changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
-
   root 'static_pages#index'
+
+  get 'static_pages/about', path: 'about'
+  get 'static_pages/contact', path: 'contact'
 end
