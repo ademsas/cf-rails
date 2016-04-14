c39b34fc502d0e7edfb011fdb66701bbf67f4e4a
Ben Clarke <ben@upliftingmedia.co.uk>
2016-04-14 01:19:53 +0100

    added color field to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -55,6 +55,6 @@ class ProductsController < ApplicationController
     end
 
     def product_params
-      params.require(:product).permit(:name, :description, :image_url)
+      params.require(:product).permit(:name, :description, :color, :image_url)
     end
 end
