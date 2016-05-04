37955433bdd3b6faeb6f92c47fe28b6f972d8522
Ben Clarke <ben@codetwentyone.co.uk>
2016-05-04 10:48:11 +0100

    view ex fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
-Rails.application.routes.draw do
+	Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
   
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
 
   get 'static_pages/about', path: 'about'
   get 'static_pages/contact', path: 'contact'
