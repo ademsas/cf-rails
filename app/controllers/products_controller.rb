8f585043318d0ab5b9e6fa5d7c85b48791441af3
Ben Clarke <ben@codetwentyone.co.uk>
2016-05-06 11:22:55 +0100

    small change

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,6 @@ class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
-
     if (Environment.current == Environment.DEVELOPMENT) {
       if params[:q]
         search_term = params[:q]
