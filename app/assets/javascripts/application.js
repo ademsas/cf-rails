4772454971aedc9de1bbe481cc7102dbb6d088a6
Ben Clarke <ben@codetwentyone.co.uk>
2016-05-10 17:28:52 +0100

    Add Raty files and added some comment files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,3 +27,13 @@ $(document).ready(function() {
     $(".alert").remove();
   })
 });
+
+$(document).on('ready page:load', function(){
+  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+  $('.rated').raty({ path: '/assets',
+    readOnly: true,
+    score: function() {
+      return $(this).attr('data-score');
+    }
+  });
+});
