d6781700a1d98079d4c0959ab5684765be18c460
Ben Clarke <ben@codetwentyone.co.uk>
2016-05-11 11:15:36 +0100

    precompile assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,11 +28,26 @@ $(document).ready(function() {
   })
 });
 
-$(document).on('ready page:load', function(){
-  $('.rating').raty( { path: '../assets', scoreName: 'comment[rating]' });
-  $('.rated').raty({ path: '../assets',
-    readOnly: true,
-    score: function() {
+// $(document).on('ready page:load', function(){
+//   $('.rating').raty( { path: '../assets', scoreName: 'comment[rating]' });
+//   $('.rated').raty({ path: '../assets',
+//     readOnly: true,
+//     score: function() {
+//       return $(this).attr('data-score');
+//     }
+//   });
+// });
+
+$(document).on('ready page:load ajaxSuccess', function(){
+  $('.rating').raty( { 
+    path: '/assets', 
+    scoreName: 'comment[rating]' 
+  });
+
+  $('.rated').raty({ 
+    path: '/assets',
+      readOnly: true,
+      score: function() {
       return $(this).attr('data-score');
     }
   });
