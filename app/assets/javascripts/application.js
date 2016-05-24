ed491ea471014dbcdade0b587988241ce5bc5f58
Ben Clarke <ben@codetwentyone.co.uk>
2016-05-24 01:09:15 +0100

    raty attempt at fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,8 +32,8 @@ $(document).ready(function() {
 
 $(document).on('ready page:load', function(){
   $('.img-zoom').elevateZoom();
-  $('.rating').raty( { path: '/assets/raty', scoreName: 'comment[rating]' });
-  $('.rated').raty({ path: '/assets/raty',
+  $('.rating').raty( { path: '../assets/raty', scoreName: 'comment[rating]' });
+  $('.rated').raty({ path: '../assets/raty',
     readOnly: true,
     score: function() {
       return $(this).attr('data-score');
